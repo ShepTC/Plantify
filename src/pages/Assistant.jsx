dbc49c61f639a01b88804235d84e6f1cfb9b320c
Shepherd Howe <howeshepherd@gmail.com>
2026-01-27 06:03:15 +0000

    File changes

--- src/pages/Assistant.jsx
+++ src/pages/Assistant.jsx
@@ -38,7 +38,7 @@ import { useToast } from "@/components/ui/use-toast";
 import { createPageUrl } from '@/components/utils';
 
 
-const GREEN_BOT_LOGO = "https://qtrypzzcjebvfcihiynt.supabase.co/storage/v1/object/public/base44-prod/public/68941e9da4c1421699b441d7/e343d3c28_GreenBotLogo.png";
+const GREEN_BOT_LOGO = "https://qtrypzzcjebvfcihiynt.supabase.co/storage/v1/object/public/base44-prod/public/68941e9da4c1421699b441d7/42d7337fe_GreenChatLogo.png";
 
 export default function Assistant() {
   const navigate = useNavigate();
